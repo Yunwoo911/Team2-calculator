a83ee7ee334173171ae03655e3a00a3f9fd99c88
hellojunho <jo62698476@gmail.com>
2024-03-11 14:43:41 +0900

    add: calculator.py commit

---
+++ calculator.py
@@ -0,0 +1,16 @@
+class Calculator:
+    def __init__(self, a, b) -> None:
+        self.a = a
+        self.b = b
+
+    def add(a, b):
+        return a + b
+    
+    def sub(a, b):
+        return a - b
+    
+    def mul(a, b):
+        return a * b
+    
+    def mod(a, b):
+        return a // b
